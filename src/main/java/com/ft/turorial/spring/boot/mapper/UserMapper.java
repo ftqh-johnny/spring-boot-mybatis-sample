6a35ec608ecbe72df8444275638d9ca5207976a7
xtiawxf <632763745@qq.com>
2016-09-23 10:12:30 +0800

    fix a problem

--- src/main/java/com/ft/turorial/spring/boot/mapper/UserMapper.java
+++ src/main/java/com/ft/turorial/spring/boot/mapper/UserMapper.java
@@ -2,9 +2,6 @@ package com.ft.turorial.spring.boot.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.ft.turorial.spring.boot.domain.User;
@@ -12,10 +9,16 @@ import com.ft.turorial.spring.boot.domain.User;
 @Mapper
 public interface UserMapper {
 	
-	@Select("select * from user where id= #{id,jdbcType=INTEGER} ")
+	/*@Results(id = "User", value = {
+			  @Result(property = "userName", column = "user_name")
+			})
+	@Select("select * from user where id= #{id,jdbcType=INTEGER} ")*/
+	@Select("select * from user where id= #{id} ")
 	User findOne(@Param("id")int id);
 	
-	@Select("select * from user where user_name= #{userName,jdbcType=VARCHAR} ")
+	//@ResultMap(value = "User")
+	//@Select("select * from user where user_name= #{userName,jdbcType=VARCHAR} ")
+	@Select("select * from user where user_name= #{userName} ")
 	User findByUserName(@Param("userName")String userName);
 	
 }
